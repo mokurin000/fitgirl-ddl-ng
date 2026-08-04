c8458086482447f5d88aa70a927319ca8458e788
莯凛 <mokurin000@gmail.com>
2026-08-04 14:16:02 +0800

    feat: export dlpass & cf_clearance cookies

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-import asyncio
 import queue
+import shutil
+import asyncio
+import tempfile
+from pathlib import Path
 
 import nodriver as uc
-from nodriver import cdp
+from nodriver import Browser, cdp
 
 BROWSER_INSTANCE = None
 DOWNLOAD_EVENTS = queue.Queue()
 
+TEMP_DIR = str(Path(tempfile.gettempdir()) / "fitgirl-ddl-ng")
+
 
 def download_handler(event: cdp.browser.DownloadWillBegin):
-    print(f"Detected download event: {event.guid}")
-    DOWNLOAD_EVENTS.put(event.guid)
+    DOWNLOAD_EVENTS.put(event)
 
 
 async def cancel_downloads():
     while True:
         try:
-            guid = DOWNLOAD_EVENTS.get_nowait()
+            event: cdp.browser.DownloadWillBegin = DOWNLOAD_EVENTS.get_nowait()
         except queue.Empty:
             await asyncio.sleep(0.0)
         else:
-            await BROWSER_INSTANCE.send(cdp.browser.cancel_download(guid))
+            await BROWSER_INSTANCE.send(cdp.browser.cancel_download(event.guid))
 
 
-async def main():
-    global BROWSER_INSTANCE
-
-    config = uc.Config()
-    browser = await uc.start(config=config)
-    BROWSER_INSTANCE = browser
-
+async def ensure_cookies(browser: Browser):
     page = await browser.get(
         "https://fuckingfast.co/oemaevh39h2t#Skills_and_Raids_--_fitgirl-repacks.site_--_.rar"
     )
-
-    await page.wait_for("div#cf-turnstile", timeout=30.0)
-    await page.verify_cf()
+    await page.verify_cf(template_image="cloudflare.png")
 
     button = await page.select("a.gay-button")
     while True:
@@ -44,17 +40,50 @@ async def main():
             break
         await asyncio.sleep(0.5)
 
-    print("Button available now!")
+    # Pop-up ads
+    await button.click()
 
+    cf_clearance = None
+    dlpass = None
+
+    while dlpass is None:
+        cookies = await browser.cookies.get_all()
+        for cookie in cookies:
+            if cookie.name == "dlpass":
+                dlpass = cookie.value
+            elif cookie.name == "cf_clearance":
+                cf_clearance = cookie.value
+        await asyncio.sleep(0.5)
+
+    return f"cf_clearance={cf_clearance}; dlpass={dlpass}"
+
+
+async def main():
+    global BROWSER_INSTANCE
+
+    browser = await uc.start()
+    BROWSER_INSTANCE = browser
+
+    asyncio.create_task(cancel_downloads())
+    await browser.send(
+        cdp.browser.set_download_behavior(
+            behavior="allowAndName",
+            download_path=TEMP_DIR,
+            events_enabled=True,
+        )
+    )
     browser.add_handler(
         cdp.browser.DownloadWillBegin,
         download_handler,
     )
 
-    # Pop-up ads
-    await button.click()
+    cookies = await ensure_cookies(browser=browser)
+    print(cookies)
 
-    await asyncio.sleep(180.0)
+    # Clean-up
+    browser.stop()
+    print(TEMP_DIR)
+    shutil.rmtree(TEMP_DIR)
 
 
 if __name__ == "__main__":
