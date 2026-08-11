bf0e56caa198a6892cf9d850445b917bb0c2869f
莯凛 <mokurin000@gmail.com>
2026-08-11 08:21:33 +0800

    build: deploy GUI version

--- scripts/gui/deploy.sh
+++ scripts/gui/deploy.sh
@@ -1,3 +1,23 @@
+BIN_NAME=fitgirl_ddl_ngui
+BIN_FILE="target/release/${BIN_NAME}.exe"
+
+(
+    if ! [ -d "launcher" ]; then
+        git clone --depth 1 \
+            https://github.com/mokurin000/pyo3-simple-launcher \
+            launcher
+    fi
+
+    cd launcher
+
+    if ! [ -f "${BIN_FILE}" ]; then
+        export CARGO_TARGET_DIR=target
+
+        uv run generate.py "${BIN_NAME}" fitgirl_ddl_ng.gui.__main__:main
+        cargo +nightly build --release
+    fi
+)
+
 mkdir -p deploy-gui && cd deploy-gui || exit 1
 
 python_ver=3.14
@@ -9,19 +29,23 @@ python_zip="python-${python_full_ver}-embed-amd64"
 if ! [ -f "${python_zip}.zip" ]; then
     curl -LO "https://www.python.org/ftp/python/${python_full_ver}/${python_zip}.zip"
 fi
+
 python_dir="python-embed-amd64"
 rm -rf "${python_dir}" || exit 1
-mkdir -p "${python_dir}" && cd "${python_dir}" || exit 1
-unzip "../${python_zip}.zip"
 
-# Enable the site-packages support
-sed -i 's/#import site/import site/g' python3*._pth
-# Install dependencies
-pip3.14 install "../..[gui]" -t Lib/site-packages
+(
+    mkdir -p "${python_dir}" && cd "${python_dir}" || exit 1
+    unzip "../${python_zip}.zip"
 
-# clean-up *.pyc
-for dir in "__pycache__" "tests"; do
-    find "Lib/site-packages" -name "$dir" -type d | xargs rm -rf
-done
+    # Enable the site-packages support
+    sed -i 's/#import site/import site/g' python3*._pth
+    # Install dependencies
+    pip3.14 install "../..[gui]" -t Lib/site-packages
 
-# TODO: GUI launcher without flashing console
+    # clean-up *.pyc
+    for dir in "__pycache__" "tests"; do
+        find "Lib/site-packages" -name "$dir" -type d | xargs rm -rf
+    done
+)
+
+cp "../launcher/${BIN_FILE}" .
--- scripts/gui/package.sh
+++ scripts/gui/package.sh
@@ -4,6 +4,6 @@ cd deploy-gui || exit 1
 rm -rf ${slug} || exit 1
 
 mkdir ${slug}
-mv python-embed-amd64 *.bat ${slug}/
+mv python-embed-amd64 *.exe ${slug}/
 
 7z a -sfx7z.sfx -t7z -mx=9 -m0=lzma2 -md=256m -ms=on ${slug}.exe ${slug}/
