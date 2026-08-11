e5b1cfa468986355cf6d90892986abaf2408ac80
莯凛 <mokurin000@gmail.com>
2026-08-11 08:38:35 +0800

    build: repackage support

--- scripts/gui/package.sh
+++ scripts/gui/package.sh
@@ -1,9 +1,11 @@
 slug=fitgirl-ddl-ng
 
 cd deploy-gui || exit 1
-rm -rf ${slug} || exit 1
+mkdir -p ${slug}
 
-mkdir ${slug}
-mv python-embed-amd64 *.exe ${slug}/
+if [ -d "python-embed-amd64" ]; then
+    rm -rf ${slug}/* || exit 1
+    mv python-embed-amd64 *.exe ${slug}/
+fi
 
 7z a -sfx7z.sfx -t7z -mx=9 -m0=lzma2 -md=256m -ms=on ${slug}.exe ${slug}/
