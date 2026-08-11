c28bf7b62b1a1f8497dc49d5e0d4e9bc26c05dcf
莯凛 <mokurin000@gmail.com>
2026-08-11 18:26:10 +0800

    build: package GUI using `._pth`

--- scripts/gui/deploy.sh
+++ scripts/gui/deploy.sh
@@ -1,6 +1,10 @@
 BIN_NAME=fitgirl_ddl_ngui
 BIN_FILE="target/release/${BIN_NAME}.exe"
 
+python_ver=3.14
+python_full_ver=${python_ver}.7
+python_num_ver=$(echo -n $python_ver | tr -cd '0-9')
+
 (
     if ! [ -d "launcher" ]; then
         git clone --depth 1 \
@@ -9,19 +13,16 @@ BIN_FILE="target/release/${BIN_NAME}.exe"
     fi
 
     cd launcher
+    [ -d ".git" ] && git pull
 
-    if ! [ -f "${BIN_FILE}" ]; then
-        export CARGO_TARGET_DIR=target
+    export CARGO_TARGET_DIR=target
 
-        uv run generate.py "${BIN_NAME}" fitgirl_ddl_ng.gui.__main__:main
-        cargo +nightly build --release
-    fi
+    uv run generate.py "${BIN_NAME}" fitgirl_ddl_ng.gui.__main__:main
+    cargo +nightly build --release
 )
 
 mkdir -p deploy-gui && cd deploy-gui || exit 1
 
-python_ver=3.14
-python_full_ver=${python_ver}.7
 python_zip="python-${python_full_ver}-embed-amd64"
 
 # Download the latest python embed 3.14 build here
@@ -30,22 +31,29 @@ if ! [ -f "${python_zip}.zip" ]; then
     curl -LO "https://www.python.org/ftp/python/${python_full_ver}/${python_zip}.zip"
 fi
 
-python_dir="python-embed-amd64"
-rm -rf "${python_dir}" || exit 1
+output_dir="Lib"
+rm -rf "${output_dir}" || exit 1
 
 (
-    mkdir -p "${python_dir}" && cd "${python_dir}" || exit 1
+    mkdir -p "${output_dir}" && cd "${output_dir}" || exit 1
     unzip "../${python_zip}.zip"
-
-    # Enable the site-packages support
-    sed -i 's/#import site/import site/g' python3*._pth
-    # Install dependencies
-    pip3.14 install "../..[gui]" -t Lib/site-packages
-
-    # clean-up *.pyc
-    for dir in "__pycache__" "tests"; do
-        find "Lib/site-packages" -name "$dir" -type d | xargs rm -rf
-    done
 )
+
+# Install dependencies
+pip3.14 install "..[gui]" -t Lib/site-packages
+
+# Clean-up *.pyc
+for dir in "__pycache__" "tests"; do
+    find "Lib/site-packages" -name "$dir" -type d | xargs rm -rf
+done
+# Remove python interceptors
+rm Lib/python{,w}.exe Lib/python${python_num_ver}._pth
+mv Lib/python${python_num_ver}.dll .
+
+cat > "python${python_num_ver}._pth" <<EOF
+Lib/
+Lib/python${python_num_ver}.zip
+import site
+EOF
 
 cp "../launcher/${BIN_FILE}" .
--- scripts/gui/package.sh
+++ scripts/gui/package.sh
@@ -3,9 +3,9 @@ slug=fitgirl-ddl-ng
 cd deploy-gui || exit 1
 mkdir -p ${slug}
 
-if [ -d "python-embed-amd64" ]; then
+if [ -d "Lib" ]; then
     rm -rf ${slug}/* || exit 1
-    mv python-embed-amd64 *.exe ${slug}/
+    mv Lib fitgirl_ddl_ngui.exe python*.dll python*._pth ${slug}/
 fi
 
 7z a -sfx7z.sfx -t7z -mx=9 -m0=lzma2 -md=256m -ms=on ${slug}.exe ${slug}/
