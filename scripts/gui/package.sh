4e17b7d79bfac0e6ae06315a4f95b9f013833e6b
莯凛 <mokurin000@gmail.com>
2026-08-11 19:15:03 +0800

    build: remove the previously packged exe

--- scripts/gui/package.sh
+++ scripts/gui/package.sh
@@ -8,4 +8,5 @@ if [ -d "Lib" ]; then
     mv Lib fitgirl_ddl_ngui.exe python*.dll python*._pth ${slug}/
 fi
 
+rm -rf "${slug}.exe"
 7z a -sfx7z.sfx -t7z -mx=9 -m0=lzma2 -md=256m -ms=on ${slug}.exe ${slug}/
